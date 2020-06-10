d9a6e049f249e69cb18185baf61f05af71572814
Samuel Collet <samuelpcollet@gmail.com>
2020-06-10 14:32:55 -0700

    switching libs

--- source/ingestion/kafka_consumer.py
+++ source/ingestion/kafka_consumer.py
@@ -33,8 +33,8 @@ def consume(db, topic='topic'):
     )
     try:
         for message in consumer:
-            print(message.value)
             event = umsgpack.unpackb(message.value)
+            print(event)
             handle_event(db, event)
     except KeyboardInterrupt:
         sys.exit()
