66b9badca329f12922252647e69ea9975e68b187
Samuel Collet <samuelpcollet@gmail.com>
2020-06-09 19:28:49 -0700

    updating p

--- source/ingestion/kafka_consumer.py
+++ source/ingestion/kafka_consumer.py
@@ -28,7 +28,7 @@ def consume(db, topic='topic'):
     try:
         for message in consumer:
             event = pickle.loads(message.value)
-            print(event, pickle.loads(messge))
+            print(event, pickle.loads(message))
             handle_event(db, event)
     except KeyboardInterrupt:
         sys.exit()
