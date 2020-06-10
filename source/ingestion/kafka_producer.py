bed3437ab44af52d999c212165c244de979276b4
Samuel Collet <samuelpcollet@gmail.com>
2020-06-10 11:35:28 -0700

    named serializer

--- source/ingestion/kafka_producer.py
+++ source/ingestion/kafka_producer.py
@@ -9,12 +9,15 @@ def produce(generator, topic='topic'):
     if __name__ != "__main__":
         bootstrap_servers = ['localhost:9092']
         topic_name = topic
-        producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
         producer = KafkaProducer()
     while True:
         event = generator.generate_data()
         if __name__ != "__main__":
-            ack = producer.send(topic_name, json.dumps(event))
+            ack = producer.send(topic_name, event)
 
 if __name__ == "__main__":
     import data_generator
