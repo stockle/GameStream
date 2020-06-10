edb9cd1ef6c26ac7dcf601fed4c58e2cf081b3cd
Samuel Collet <samuelpcollet@gmail.com>
2020-06-10 11:22:07 -0700

    json serial problems dub quotes

--- source/ingestion/data_generator.py
+++ source/ingestion/data_generator.py
@@ -9,22 +9,22 @@ GAME_NAMES_PATH = "../data/vgsales.csv"
 class DataGenerator():
 	def __init__(self, num_users=6000000):
 		self.supported_platform = {
-			'PC': self.generate_pc_stats(),
-			'PS4': self.generate_ps4_stats()
+			"PC": self.generate_pc_stats(),
+			"PS4": self.generate_ps4_stats()
 		}
 		self.uids = self.generate_uids(num_users)
 		self.games = self.read_games(GAME_NAMES_PATH)
 
 	def generate_pc_stats(self):
 		return {
-			'CPU': psutil.cpu_percent(interval=1) * 8 * random(),
-			'RAM': psutil.virtual_memory()[2] * random()
+			"CPU": psutil.cpu_percent(interval=1) * 8 * random(),
+			"RAM": psutil.virtual_memory()[2] * random()
 		}
 
 	def generate_ps4_stats(self):
 		return {
-			'CPU': psutil.cpu_percent(interval=1) * 10 * random(),
-			'RAM': psutil.virtual_memory()[2] * random()
+			"CPU": psutil.cpu_percent(interval=1) * 10 * random(),
+			"RAM": psutil.virtual_memory()[2] * random()
 		}
 
 	def get_users(self):
@@ -36,14 +36,14 @@ class DataGenerator():
 			reader = csv.reader(csv_file)
 			for row in reader:
 				if row[2] in self.supported_platform:
-					games.append({'game':row[1], 'platform':row[2]})
+					games.append({"game":row[1], "platform":row[2]})
 		return games
 
 	def generate_uids(self, num_users):
 		users = [{
-			'UID': str(uuid.uuid4()),
-			'Age': self.generate_age(),
-			'StartedPlaying': None
+			"UID": str(uuid.uuid4()),
+			"Age": self.generate_age(),
+			"StartedPlaying": None
 		} for i in range(num_users)]
 
 		return users
@@ -53,14 +53,14 @@ class DataGenerator():
 
 	def get_uid(self):
 		user = choice(self.uids)
-		if user['StartedPlaying']:
-			delta = datetime.now() - user['StartedPlaying']
+		if user["StartedPlaying"]:
+			delta = datetime.now() - user["StartedPlaying"]
 			r = gauss(0.5, 0.4)
 			if (delta.seconds * delta.days > 30 * 60 and r < 0.8) or r < 0.2:
-				user['StartedPlaying'] = None
+				user["StartedPlaying"] = None
 		else:
-			user['StartedPlaying'] = datetime.now()
-		return user['UID']
+			user["StartedPlaying"] = datetime.now()
+		return user["UID"]
 
 	def generate_age(self):
 		return int(gauss(0.75, 0.15) * uniform(22, 70))
@@ -70,30 +70,30 @@ class DataGenerator():
 
 	def create_purchase_event(self):
 		game = self.get_game()
-		platform = game['platform']
+		platform = game["platform"]
 		platform_stats = self.generate_platform_stats(platform)
 
 		return {
-			'Game': game['game'],
-			'Platform': platform,
-			'PlatformStats': platform_stats,
+			"Game": game["game"],
+			"Platform": platform,
+			"PlatformStats": platform_stats,
 		}
 
 	def create_gameplay_event(self):
 		game = self.get_game()
-		platform = game['platform']
+		platform = game["platform"]
 		platform_stats = self.generate_platform_stats(platform)
 
 		return {
-			'Game': game['game'],
-			'Platform': platform,
-			'PlatformStats': platform_stats,
+			"Game": game["game"],
+			"Platform": platform,
+			"PlatformStats": platform_stats,
 		}
 
 	def create_event(self):
 		if random() < 0.08:
-			return 'purchase_event', self.create_purchase_event()
-		return 'gameplay_event', self.create_gameplay_event()
+			return "purchase_event", self.create_purchase_event()
+		return "gameplay_event", self.create_gameplay_event()
 
 	def generate_data(self):
 		event_type, event = self.create_event()
@@ -101,9 +101,9 @@ class DataGenerator():
 		time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
 
 		return {
-			'event_type': event_type,
-			'UID': uid,
-			'event_body': event
+			"event_type": event_type,
+			"UID": uid,
+			"event_body": event
 		}
 
 if __name__=="__main__":
