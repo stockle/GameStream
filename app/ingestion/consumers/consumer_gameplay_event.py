1511499d608dff27fb7a0bc0daa20135245210c4
Samuel Collet <samuelpcollet@gmail.com>
2020-06-11 17:45:13 -0700

    wrong table name

--- app/ingestion/consumers/consumer_gameplay_event.py
+++ app/ingestion/consumers/consumer_gameplay_event.py
@@ -2,7 +2,7 @@ import json
 
 def handle_gameplay_event(db, event):
     query = """
-        INSERT INTO purchase_events
+        INSERT INTO gameplay_events
         (user_id, event_time, game, platform, platform_stats)
         VALUES (%s,%s,%s,%s,%s)
     """
