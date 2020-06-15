e75f5e2e9cab275a855c238a8638430120825632
Samuel Collet <samuelpcollet@gmail.com>
2020-06-15 16:08:52 -0700

    starting to process form values

--- app/app.py
+++ app/app.py
@@ -9,30 +9,39 @@ db = connector.DBConnector()
 db.init_session()
 db.init_keyspace('v1')
 
-values = db.select("""
-    SELECT * FROM gameplay_events
-    WHERE platform = 'PC'
-    ALLOW FILTERING;
-""")
-
-line_values = []
-df = pd.DataFrame(list(values))
-df['event_time'].astype('datetime64')
-lines = df.groupby(pd.Grouper(key='event_time', freq='100ms')).event_time.agg('count').to_frame('count').reset_index()
-line_values = lines['count'].values
-labels = lines['event_time'].values
-
-print(labels)
-
 colors = [
     "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
     "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
     "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
 
+def construct_query(data):
+    query = """
+        SELECT * FROM gameplay_events
+        WHERE platform = 'PC'
+        ALLOW FILTERING;
+    """
+    return query
+
+def spark_submit_query(query):
+    values = db.select()
+    line_values = []
+    df = pd.DataFrame(list(values))
+    df['event_time'].astype('datetime64')
+    lines = df.groupby(pd.Grouper(key='event_time', freq='100ms')).event_time.agg('count').to_frame('count').reset_index()
+    values = lines['count'].values
+    labels = lines['event_time'].values
+
+    print(labels)
+
+    return labels, values
+
 @app.route('/', methods=["GET", "POST"])
-def line():
-    line_labels=labels
-    return render_template('line_chart.html', title='Users per 100ms', max=max(line_values), labels=line_labels, values=line_values)
+def handle_form_submit():
+    form_data = request.form
+    print(form_data)
+    query = construct_query(form_data)
+    labels, values = spark_submit_query(query)
+    return render_template('index.html', title='PC Users per 100ms', max=max(values) + 1, labels=labels, values=values)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
