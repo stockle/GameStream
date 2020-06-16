c546c700a6e35514ec1932d46a6bcf4f2c56cc56
Samuel Collet <samuelpcollet@gmail.com>
2020-06-16 14:46:34 -0700

    testing sql joins

--- app/database/spark_connector.py
+++ app/database/spark_connector.py
@@ -18,8 +18,8 @@ class SparkConnector:
 		return self.sqlContext.sql(query).collect()
 
 	def load_and_get_table_df(self, keys_space_name, table_name):
-		table_df = self.sqlContext.read\
-			.format("org.apache.spark.sql.cassandra")\
-			.options(table=table_name, keyspace=keys_space_name)\
+		table_df = self.sqlContext.read \
+			.format("org.apache.spark.sql.cassandra") \
+			.options(table=table_name, keyspace=keys_space_name) \
 			.load()
 		return table_df
