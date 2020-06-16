b60b30bf83009a1a1f97e536c95455014cf4ab86
Samuel Collet <samuelpcollet@gmail.com>
2020-06-16 10:43:15 -0700

    indent

--- app/database/spark_connector.py
+++ app/database/spark_connector.py
@@ -5,14 +5,14 @@ from pyspark_cassandra import CassandraSparkContext
 
 class SparkConnector:
 	def __init__(self):
-		conf = SparkConf \
-			.setMaster(os.environ['DB_ADDR']) \
+		conf = SparkConf() \
+			.setMaster("spark://spark-master:7077") \
 			.setAppName('SparkCassandraAnalytics') \
 			.set('spark.cassandra.connection.host', os.environ['DB_ADDR']) \
 			.set('spark.cassandra.auth.username', os.environ['DB_USER']) \
 			.set('spark.cassandra.auth.password', os.environ['DB_PASS']) \
 			.getOrCreate()
-		self.sc = CassandraSparkContext(conf=conf)
+		self.sc = SparkContext(conf=conf)
 		self.sqlContext = SQLContext(sc)
 
 	def submit_sql(self, query):
