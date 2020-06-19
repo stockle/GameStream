5645bdce40ce4d77dacb1288b134ca9655206e3c
Samuel Collet <samuelpcollet@gmail.com>
2020-06-18 20:47:22 -0700

    m

--- app/app.py
+++ app/app.py
@@ -101,7 +101,9 @@ def submit_query(queries):
     pevents['count'] = pevents.groupby(pd.Grouper(key='event_time', freq='60s')).event_time.transform('nunique')#.count().reset_index(name="count")
     gevents['count'] = gevents.groupby(pd.Grouper(key='event_time', freq='60s')).event_time.transform('nunique')#.count().reset_index(name="count")
 
-    values = pd.merge(gevents, pevents, on='event_time').sort_values(by='event_time')
+    values = pd.merge(gevents, pevents, on='event_time')#
+    print(values)
+    values = values.sort_values(by='event_time')
     users = users[~users['id'].isin(values)].groupby(['min_age', 'max_age']).size().unstack(fill_value=0).stack().reset_index(name='count')
 
     return {
